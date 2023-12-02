fc310dd8f0fdf9fb2fce3e0539a1d2a98fa5a336
The_Valkyrie <110331611+Kirieshek@users.noreply.github.com>
2023-12-02 12:16:42 +0300

    Add files via upload

---
+++ homework/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+    {{ text }} <br>
+    <br>
+    <button @click="change">Изменить содержимое текста</button><br>
+    <button @click="say('Привет')">Изменить содержимое текст на "Привет"</button><br>
+    <button @click="say('Пока')">Изменить содержимое текста на "Пока"</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: 'xxx'
+    }
+  },
+  methods: {
+    change() {
+      this.text = 'yyy' 
+    },
+    say(text) {
+      this.text = text 
+    }
+  }
+}
+</script>
+
+<style>
+  
+</style>
